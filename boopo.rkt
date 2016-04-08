363126c842cad3cbe7b21ddf7cee9d294529f4bc
interplinternet <astro@airmail.cc>
2016-04-08 15:36:48 -0700

    Add some more 2D vector operations
    This includes vec-, which subtracts 2 vectors; vec-max, which consumes two vectors and returns the first if its magnitude is larger than or equal to the second; vec-min, which does similarly but if the first is strictly smaller than the second; and heading, which returns the heading angle of a vector between 2 points.

--- boopo.rkt
+++ boopo.rkt
@@ -21,6 +21,12 @@
 ;   is this really necessary? Since velocity is directed, isn't it redundant to
 ;   mention direction twice?
 ; -- l is the player's location, expressed as an angle and magnitude from the upper left
+
+(struct game (p t)) ; contains a player and the turret
+
+; A turret could be:
+; 1. a pvec, representing it's x- and y-coords.
+; 2. a struct, containing a pvec and it's rotation and firing information
 ; -------------------------------------------------------------------------------------
 #| DATA EXAMPLES |#
 ; -------------------------------------------------------------------------------------
@@ -46,10 +52,22 @@
 ; -> Player
 ; the game's state, for now, is just a player
 (define (start)
-  (big-bang (player (pvec 0 0) 0 (pvec (random WIDTH) (random HEIGHT)))
+  (big-bang (init-game)
             [on-key direct-ship]
             [on-tick fly-ship]
             [to-draw render-game]))
+
+; -> Game
+(define (init-game)
+  (game (init-player) (init-turret)))
+
+; -> Player
+(define (init-player)
+  (player (pvec 0 0) 0 (pvec (random WIDTH) (random HEIGHT))))
+
+; -> Turret
+(define (init-turret)
+  (pvec (random WIDTH) (random HEIGHT)))
 
 ; -------------------------------------------------------------------------------------
 #| LOGIC |#
@@ -60,6 +78,43 @@
   (pvec
    (+ (pvec-x v1) (pvec-x v2))
    (+ (pvec-y v1) (pvec-y v2))))
+
+; Pvec Pvec -> Rational
+; returns the heading of a line between two vector points
+(define (heading v1 v2)
+  (define max_v (vec-max v1 v2))
+  (define min_v (vec-min v1 v2))
+  ; - IN -
+  ; take the inverse tangent of the difference between two points
+  (atan (vec- max_v min_v)))
+
+; Pvec Pvec -> Pvec
+(define (vec-max v1 v2)
+  (define origin (pvec 0 0))
+  (if (>= (vec-mag v1 origin)
+          (vec-mag v2 origin))
+      v1
+      v2))
+
+; Pvec Pvec -> Pvec
+(define (vec-min v1 v2)
+  (define origin (pvec 0 0))
+  (if (< (vec-mag v1 origin)
+         (vec-mag v2 origin))
+      v1
+      v2))
+
+; Pvec Pvec -> Pvec
+(define (vec- v1 v2)
+  (pvec
+   (- (pvec-x v1) (pvec-x v2))
+   (- (pvec-y v1) (pvec-y v2))))
+
+; Pvec Pvec -> Number
+; b/c we represent points as 2D vectors as well, our magnitude must take this into account
+(define (vec-mag v1 v2)
+  (sqrt (+ (sqr (- (pvec-x v1) (pvec-x v2)))
+           (sqr (- (pvec-y v1) (pvec-y v2))))))
 
 ; later on, add some other vector functions that will be useful, like computing the heading as an angle, computing it's magnitude, etc.
 ; Player KeyEvent -> Player
