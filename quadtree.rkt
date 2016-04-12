53fef83d38f157e583a39bf37220d7b2cbd121b5
interplinternet <astro@airmail.cc>
2016-04-11 23:54:00 -0700

    Refactoring
    Refactored the folding function in make-empty-quadtree into it's own locally defined function. Adding some notes about make-squares. It looks like foil-distribution. There should be a way to automate that kind of traversal, but I haven't figured it out. Experimented and pasted some snippets into a separate .org file.

--- quadtree.rkt
+++ quadtree.rkt
@@ -31,30 +31,36 @@
 ;-------------------------------------------------------------------------------------
 #| LOGIC |#
 ;-------------------------------------------------------------------------------------
-; for easy prototyping, we use lists to represent the tree.
+; for easy prototyping, we use lists to represent the tree. The nodes contain
+; nothing except for their coordinates and their children
+
+; Node := [List [List Number Number] [Listof Node]]
 ; Number Number Number Number -> Quadtree
 (define (make-empty-tree x y dimension depth)
+
+  ; [Listof [List Number Number]] -> Node
+  (define (make-sub-trees mid lo-sqr)
+    (foldr (λ (a-sqr rst)
+             (list* a-sqr
+                    (make-empty-tree (first a-sqr) (second a-sqr) mid (sub1 depth))
+                    rst))
+           '()
+           lo-sqr))
   (cond
     [(zero? depth) '()]
     [else (define mid (/ dimension 2))
           (define new-squares (make-squares x y mid))
-          (foldr (λ (a-sqr rst)
-                   (list* a-sqr
-                          (make-empty-tree
-                           (first a-sqr) (second a-sqr) mid (sub1 depth))
-                          rst))
-                 '()
-          new-squares)]))
-          #| (for/fold ([acc   '()])
-                    ([a-sqr new-squares])
-            (list* a-sqr
-                   (make-empty-tree (first a-sqr) (second a-sqr) mid (sub1 depth))
-                   acc))])) |#
+          ; - IN -
+          (make-sub-trees mid new-squares)]))
 
 ; Number Number Number -> [Listof Number Number]
 (define (make-squares x y dimension)
   ; isn't the dimension half of the largest value?
   ; i.e., in (0, 1) => .5, (2, 3) -> 2
+  ; this function feels very "off". Isn't there a way to traverse the list
+  ; and create the values?
+  ; Steps: Add dimension to none, then first, then second, then both values.
+  ; (x y) (fx y) (x fy) (fx fy), looks kind of like FOIL-distribution.
   (list (list x y)
         (list (+ x dimension) y)
         (list x (+ y dimension))
@@ -68,4 +74,3 @@
 
 (define bound-4-tree
   (cons '(0 0) (combinations (build-list 4 identity) 2)))
-
