71a28f6cf1184ef2b4d76f89a26b0ef96022f9ca
interplinternet <astro@airmail.cc>
2016-04-12 18:59:41 -0700

    Cleanup, add insertion/retrieval functions.
    Nodes now include content, but are empty lists by default. Added some comments about using vectors instead of lists in order to take advantage of indices. The 0 index could be the upper-left, the 1 index could be the upper right, etc. until the 3 index is the lower right. I simplified make-subtrees. I didn't need to use folding, just a map function.
    
    Zero depth is now corrected and represents the root node.
    
    Added functions for retrieving and inserting nodes. The functions are first-come first-serve and need to be fixed, but this is just the prototype. They need to be fixed to insert and retrieve to appropriate nodes, probably by taking dimension or index into account. This is a result of the ambiguity resulting from allowing the upper-left point to represent the entire quad. For example, the point (0, 0) represents every node starting at that point including the largest (the entire screen/root-node) and the smallest (an infinitesimally small quad).

--- quadtree.rkt
+++ quadtree.rkt
@@ -5,8 +5,8 @@
 ;-------------------------------------------------------------------------------------
 (struct posn (x y) #:transparent)
 
-(struct node (coord children) #:transparent)
-; A Node is (node (posn #(node node node node)))
+(struct node (coord content children) #:transparent)
+; A Node is (node (posn Number Number) Any [List Node Node Node Node])
 
 ;-------------------------------------------------------------------------------------
 #| CONSTANTS |#
@@ -21,37 +21,44 @@
 (define MAX-THINGS '(flying-ship other-ship uhh some-thing))
 (define ONE-THING '(flying-ship))
 (define TWO-THINGS '(flying-ship other-ship))
-(define root (node (posn 0 0) '())) ; a node with no entities
-(define root1 (node (posn 0 0) '(flying-ship)))
-(define root2 (node (posn 0 0) MAX-THINGS)) ;max-nodes, split!
+(define root (node (posn 0 0) '() '())) ; a node with no entities
+(define root1 (node (posn 0 0) '(flying-ship) '()))
+(define root2 (node (posn 0 0) MAX-THINGS '())) ;max-nodes, split!
 #;(define root3 (node (posn 0 0) (list (node (posn 0 0) ...)
                                      (node (posn 0 2) ...)
                                      (node (posn 2 0) ...)
                                      (node (posn 2 2) ...))))
+
+
 ;-------------------------------------------------------------------------------------
 #| LOGIC |#
 ;-------------------------------------------------------------------------------------
 ; for easy prototyping, we use lists to represent the tree. The nodes contain
-; nothing except for their coordinates and their children
+; nothing except for their coordinates and their children.
+
+; If we use vectors, we can use the vector's index to represent the quadrants
+; of the screen or area.
+; 0 = Upper-left, 1 = Upper-right, 2 = Lower-left, 3 = Lower-right.
 
 ; Node := [List [List Number Number] [Listof Node]]
-; Number Number Number Number -> Quadtree
-(define (make-empty-tree x y dimension depth)
-
+; Posn Number Number -> Quadtree
+; Depth represents levels past the root node. A depth of 0 is just the root node.
+(define (make-empty-tree coords dimension depth)
   ; [Listof [List Number Number]] -> Node
+  ; this could be inlined, but having it separate makes it a little easier
+  ; to understand what it does.
   (define (make-sub-trees mid lo-sqr)
-    (foldr (λ (a-sqr rst)
-             (list* a-sqr
-                    (make-empty-tree (first a-sqr) (second a-sqr) mid (sub1 depth))
-                    rst))
-           '()
-           lo-sqr))
+    (map (λ (a-sqr) (make-empty-tree a-sqr mid (sub1 depth)))
+         lo-sqr))
+  ; - IN -
   (cond
-    [(zero? depth) '()]
+    [(zero? depth) (node coords '() '())]
     [else (define mid (/ dimension 2))
-          (define new-squares (make-squares x y mid))
+          (define new-squares (make-squares (posn-x coords) (posn-y coords) mid))
+          (define subtrees (filter (λ (i) (not (empty? i)))
+                                   (make-sub-trees mid new-squares)))
           ; - IN -
-          (make-sub-trees mid new-squares)]))
+    (node coords '() subtrees)]))
 
 ; Number Number Number -> [Listof Number Number]
 (define (make-squares x y dimension)
@@ -61,16 +68,54 @@
   ; and create the values?
   ; Steps: Add dimension to none, then first, then second, then both values.
   ; (x y) (fx y) (x fy) (fx fy), looks kind of like FOIL-distribution.
-  (list (list x y)
-        (list (+ x dimension) y)
-        (list x (+ y dimension))
-        (list (+ x dimension) (+ y dimension))))
+  (list (posn x y)
+        (posn (+ x dimension) y)
+        (posn x (+ y dimension))
+        (posn (+ x dimension) (+ y dimension))))
+
+(define empty-tree (make-empty-tree (posn 0 0) 2 2))
+
+; Posn Node -> [Maybe Any]
+; Question: Do we retrieve the first node in which the coordinate exists,
+; or the last? (i.e., the largest possible area or the smallest? If the coord is 0,0
+; then we could be retrieving the entire screen, OR an infinitesimaly small part
+; in the upper left corner). We might need to take dimension into account as well.
+(define (retrieve-node coords tree)
+  ; [Listof Node] -> Any
+  (define (search-in subtree)
+    (cond
+      [(empty? subtree) #f]
+      [else
+       (define candidate (retrieve-node coords (first subtree)))
+       (if candidate candidate (search-in (rest subtree)))]))
+  (cond
+    [(posn=? coords (node-coord tree)) (node-content tree)]
+    [(cons? (node-children tree))
+     ;(ormap (λ (child) (retrieve-node coords child)) (node-children tree))]))
+     ; depth-first searching for nodes
+     (search-in (node-children tree))]
+    [(empty? (node-children tree)) #f]))
+
+; Content Posn Node -> Node
+; As a prototype, it's fine for this function to assume that we already have the coords
+; of the appropriate node. See above for problems.
+(define (insert-node entity coords tree)
+  (define (sub-insert subtree)
+    (map (λ (child) (insert-node entity coords child)) subtree))
+  ; - IN -
+  (cond
+    [(posn=? coords (node-coord tree))
+     (struct-copy node tree
+                  [content entity])]
+    [(cons? (node-children tree))
+     (node (node-coord tree) (node-content tree)
+           (sub-insert (node-children tree)))]
+    [else tree]))
+
+; Posn Posn -> Boolean
+(define (posn=? p1 p2)
+  (and (= (posn-x p1) (posn-x p2))
+       (= (posn-y p1) (posn-y p2))))
 
 (define (build-lazy n proc)
   (build-list n (λ (i) (delay (proc i)))))
-
-(define possible-points
-  (combinations (build-list 4 identity) 2))
-
-(define bound-4-tree
-  (cons '(0 0) (combinations (build-list 4 identity) 2)))
