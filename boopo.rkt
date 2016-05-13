5d073bacd79941d850875dfecd246dc9708bd622
interplinternet <astro@airmail.cc>
2016-05-13 13:35:57 -0700

    Fix turret rotation, obstacles.
    The turret is now a separate data structure extending the entity struct.
    It contains the turret's rotation and firing information. The base
    obstacle image is now placed over a transparent image, which is then
    overlayed on the background during gameplay. I added some comments about
    the problems of relative vs. absolute coordinates and obstacles. The
    screen is "relative", where the upper left is always 0 and lower right
    always 720, but the background image is much larger than that and
    scrolls. Turret rotation is no longer a purely graphical change, now
    being recorded in its struct for firing direction.
    
    Readme reflects new thoughts on changes, and new tasks to do.

--- boopo.rkt
+++ boopo.rkt
@@ -1,5 +1,5 @@
 #lang racket
-(require 2htdp/image 2htdp/universe "quadtree.rkt")
+(require 2htdp/image 2htdp/universe "quadtree.rkt" racket/trace)
 
 ; -------------------------------------------------------------------------------------
 #| PHYSICAL CONSTANTS |#
@@ -14,15 +14,12 @@
 ; -------------------------------------------------------------------------------------
 #| VISUAL CONSTANTS |#
 ; -------------------------------------------------------------------------------------
-; (define BACKG    (empty-scene WIDTH HEIGHT))
 (define SPACE    (bitmap/file "graphics/big_space.bmp"))
 (define BACKG    SPACE)
-; (define SHIP     (star-polygon (/ 720 15) 5 1 'solid 'thistle))
-; (define SHIP     (triangle/sss 50 50 20 'solid 'thistle))
-(define SHIP     (rotate 180   (bitmap/file "graphics/player-ship.png")))
-(define TURRET   (overlay/align 'right 'center
-                                (circle 15 'solid 'orange)
-                                (rectangle 60 15 'solid 'orange)))
+(define SHIP     (rotate -90 (bitmap/file "graphics/player-ship.png")))
+(define TURRET   (rotate 270 (overlay/align 'right 'center
+                                         (circle 15 'solid 'orange)
+                                         (rectangle 60 15 'solid 'orange))))
 (define PWIDTH   (image-width SHIP))
 (define PHEIGHT  (image-height SHIP))
 (define BWIDTH   (image-width BACKG))
@@ -31,7 +28,9 @@
 (define BYCENTER (/ BHEIGHT 2))
 (define INIT_X   (/ BWIDTH 4)) ; the initial x and y coordinates on the background
 (define INIT_Y   (/ BHEIGHT 4))
-
+(define MAX-OBST 12)
+(define MIN-OBST 6)
+(define VROOT (node (posn INIT_X INIT_Y) '() #()))
 ; -------------------------------------------------------------------------------------
 #| DATA |#
 ; -------------------------------------------------------------------------------------
@@ -52,20 +51,21 @@
 ; -- v is the player's velocity
 ; -- l is the player's location
 
-(struct game (p t q) #:transparent)
-; (game Player Entity Quadtree)
-; contains the player, the turret, and a quadtree for collision detection.
+(struct game (p t o q) #:transparent)
+; (game Player Entity [Listof Entity] Quadtree)
+; contains the player, the turret, a list of obstacles, and a quadtree for collision detection.
 
-; A turret could be:
-; 1. a pvec, representing it's x- and y-coords.
-; 2. a struct, containing a pvec and it's rotation and firing information
+(struct turret entity (rotat fire))
+; turret = (player (posn Rational Rational) Integer Integer Integer Rational Integer
+; in (turret posn r f)
+; -- Posn is the turret's location.
+; -- R is the turret's current rotation, in radians.
+; -- F is either a number between -3 and 0, signifying the countdown till firing,
+;    or a list of fired projectiles.
 
 ; -------------------------------------------------------------------------------------
 #| DATA EXAMPLES |#
 ; -------------------------------------------------------------------------------------
-#;(define ex-p
-    (player (entity (posn 360 360) (image-width SHIP) (image-height SHIP))
-            0 (pvec 0 10) 0))
 (define ex-p
   (player (posn 360 360) PWIDTH PHEIGHT
           1 (pvec 0 1) 0))
@@ -76,7 +76,8 @@
 ; -> Game
 (define (start)
   (define the-game (init-game))
-  (define NODE (game-q the-game))
+  (define NODE     (game-q the-game))
+  (define obst-img (render-obst (game-o the-game) (empty-scene WIDTH HEIGHT 'transparent)))
 
   ; wrap the handlers with another function so they can use the base NODE with
   ; the turret to insert into.
@@ -85,33 +86,113 @@
   (define (direct-with-base game key)
     (direct-game game key NODE))
 
+  ; wraps the renderer with another function, so it doesn't have to re-render
+  ; the static obstacles every-time
+  (define (render-with-base game)
+    (render-game game obst-img))
+
   (big-bang the-game
-            [on-key  direct-with-base]
-            [on-tick update-with-base]
-            [to-draw render-game]
-            [state   #t]))
+            [on-key    direct-with-base]
+            [on-tick   update-with-base]
+            [to-draw   render-with-base]
+            [stop-when game-over?]
+            [state     #f]))
 
 ; -> Game
 (define (init-game)
-  (define turret (init-turret))
+  (define turret    (init-turret))
+  (define obstacles (init-obst))
+  ; The posn of every obstacle is "absolute", whereas the quadtree is relative.
+  ; That means obstacles are never inserted into the appropriate node! They're often
+  ; beyond the bounds of the the quad entirely, which is the WIDTH of the screen, not the
+  ; entire background which scrolls and changes. The other problem is in scrolling. If we
+  ; fold all obstacles into the quadtree at once, then they never change as they form the
+  ; new root (this problem also exists with the turret). However, as the background
+  ; scrolls, the obstacles and turrets are no longer in the same quads as they were in the
+  ; beginning! Scrolling backgrounds, while pretty, create a disparity between the
+  ; absolute of the background image and the relative coordinates of the quad.
+  ; Solutions? Reinsert everything every tick. Or create a single "absolute" quadtree
+  ; which is done over the entire background image, of which the player's screen is only a
+  ; small part.
+  (define base      (insert-node VROOT turret WIDTH))
+  (define new-root  (foldl (λ (an-entity tree) (insert-node tree an-entity WIDTH))
+                           base
+                           obstacles))
   ; the game doesn't insert the player into the quadtree until after they move,
   ; the turret is inserted before play b/c it never moves and can act as a
   ; "base" node for re-insertion, so we don't have to constantly reinsert it.
-  (game (init-player) turret (insert-node VROOT turret WIDTH)))
+  (game (init-player) turret obstacles new-root))
 
 ; -> Player
 (define (init-player)
   (player
    (posn 360 360) PWIDTH PHEIGHT
    0 (pvec 1 0) INIT-TURN))
-;(player 0 (pvec 0 0) (pvec (random WIDTH) (random HEIGHT))))
 
 ; -> Turret
 (define (init-turret)
-  (entity (posn (random WIDTH) (random HEIGHT))
-          (image-width TURRET) (image-height TURRET)))
+  (turret
+   (posn (random WIDTH) (random HEIGHT))
+   (image-width TURRET) (image-height TURRET)
+  0
+  '()))
 
+; -> [Listof Entity]
+; returns a list containing random number of entity between n and m
+(define (init-obst)
+  (define num-of-obst (random MIN-OBST MAX-OBST))
+  (gen-obst num-of-obst))
 
+; Number -> [Listof Entity]
+; Generate num amount of obstacles, but none may collide with one another.
+; 1. Create an obstacle at a random position. Does it collide with any other obstacle so far?
+; 2. If so, recreate it somewhere else. If not, cons it onto the list of obstacles so far.
+(define (gen-obst n0)
+  ; Number [Listof Entity] -> [Listof Entity]
+  ; The accumulator represents all the obstacles generated so far.
+  (define (gen-list num obstacles)
+    (cond
+      [(zero? num) obstacles]
+      [else
+       (define candidate (gen-random-obst))
+       (if (ormap (λ (obst) (collides? candidate obst)) obstacles)
+           (gen-list num obstacles)
+           (gen-list (sub1 num) (cons candidate obstacles)))]))
+  ; - IN -
+  (gen-list n0 '()))
+
+; Entity Entity -> Boolean
+; Maybe I can use the quadtree for this. If two generated obstacles
+; aren't in the same node, they can't collide.
+(define (collides? candidate obst)
+  (or (and (in-width? candidate obst)
+           (in-height? candidate obst))
+      (and (in-width? obst candidate)
+           (in-height? obst candidate))))
+
+; Entity Entity -> Boolean
+(define (in-width? candidate obst)
+  (in-dimension? candidate obst entity-width posn-x))
+
+; Entity Entity -> Boolean
+(define (in-height? candidate obst)
+  (in-dimension? candidate obst entity-height posn-y))
+
+; Entity Entity [Entity -> Number] [Entity -> Number] -> Boolean
+(define (in-dimension? candidate obst dimension pos)
+  (<= (pos (entity-coord candidate))
+      (pos (entity-coord obst))
+      (+ (pos (entity-coord candidate)) (dimension candidate))))
+
+; -> Entity
+(define (gen-random-obst)
+  ; a rectangle has an x- & y-coord, and a width and height.
+  (define width (random 6 33))
+  (define height (random 6 33))
+  (entity (posn (random WIDTH)
+                (random HEIGHT))
+          width
+          height))
 ; -------------------------------------------------------------------------------------
 #| LOGIC |#
 ; -------------------------------------------------------------------------------------
@@ -121,25 +202,21 @@
 (define (cos~ n)
   (rationalize (cos n) .1))
 
-; Posn Vector -> Posn
-(define (loc+ p v)
-  (posn
-   (rationalize (+ (posn-x p) (pvec-x v)) .1)
-   (rationalize (+ (posn-y p) (pvec-y v)) .1)))
-
 ;;----------VECTORS----------;;
+; Pvec Pvec [Pvec -> Pvec] -> Pvec
+(define/match (vec-oper v1 v2 func)
+  [((pvec x1 y1) (pvec x2 y2) func)
+   (pvec (rationalize (func x1 x2) .1)
+         (rationalize (func y1 y2) .1))])
+
 ; Pvec Pvec -> Pvec
 ; add 2 vectors together
 (define (vec+ v1 v2)
-  (pvec
-   (rationalize (+ (pvec-x v1) (pvec-x v2)) .1)
-   (rationalize (+ (pvec-y v1) (pvec-y v2)) .1)))
+  (vec-oper v1 v2 +))
 
 ; Pvec Pvec -> Pvec
 (define (vec- v1 v2)
-  (pvec
-   (rationalize (- (pvec-x v1) (pvec-x v2)) .1)
-   (rationalize (- (pvec-y v1) (pvec-y v2)) .1)))
+  (vec-oper v1 v2 -))
 
 ; Pvec Number -> Pvec
 ; scales a vector by a scalar
@@ -165,6 +242,7 @@
   (sqrt (+ (sqr (- (pvec-x v1) (pvec-x base)))
            (sqr (- (pvec-y v1) (pvec-y base))))))
 
+
 ; Pvec Pvec -> Pvec
 ; returns the largest of the two vectors
 (define (vec-max v1 v2)
@@ -180,23 +258,30 @@
          (vec-mag v2))
       v1
       v2))
+
+; Posn Vector -> Posn
+(define (loc+ p v)
+  (posn
+   (rationalize (+ (posn-x p) (pvec-x v)) .1)
+   (rationalize (+ (posn-y p) (pvec-y v)) .1)))
 ;;----------GAME----------;;
 ; Game KeyEvent Node-> Game
 (define (direct-game g ke root)
   (define p (game-p g))
   (define current-quad (game-q g))
   (define new-ship (direct-ship p ke))
-  (define new-turret (direct-turret (game-t g)))
 
-  (game new-ship new-turret
-        (insert-node root new-ship WIDTH)))
+  (struct-copy game g
+               [p new-ship]
+               [q (insert-node root new-ship WIDTH)]))
 
 ; Game -> Game
 (define (update-game g root)
   (collect-garbage 'incremental)
   (define new-ship (fly-ship (game-p g)))
   (game new-ship
-        (rotate-turret (game-t g))
+        (rotate-turret (game-t g) (game-p g))
+        (game-o g)
         (if (zero? (player-magni new-ship))
             (game-q g)
             (insert-node root new-ship WIDTH))))
@@ -222,12 +307,6 @@
     ["r"     (player (posn 360 360) PWIDTH PHEIGHT 0 (pvec 1 0) INIT-TURN)]
     [_       pl]))
 
-; Turret -> Turret
-; for now, there is nothing you can do with the turret. If it turns out that
-; there's never anything to do with it, I'll just remove this.
-(define (direct-turret tr)
-  tr)
-
 ; [Number -> Number] Number -> Number
 (define (intrvl proc n)
   (define new (proc n))
@@ -245,20 +324,22 @@
 
 ; Player -> Player
 (define (fly-ship pl)
-  (define vel     (player-veloc pl))
-  (define loc     (entity-coord pl))
-  (define s       (player-magni pl))
-  (define new-loc (loc+ loc (rotate-quad (vec-scale vel s))))
-  ; - IN -
-  (if (and (<= 0 (posn-x new-loc) WIDTH)
-           (<= 0 (posn-y new-loc) HEIGHT))
-      (player new-loc PWIDTH PHEIGHT
-              (player-magni pl) (player-veloc pl) (player-turns pl))
-      pl))
+  (match pl
+    [(player pos width height speed veloc turns)
+     (=> out-of-bounds)
+     (define new-loc (loc+ pos (rotate-quad (vec-scale veloc speed))))
+     (if (and (<= 0 (posn-x new-loc) WIDTH) (<= 0 (posn-y new-loc) HEIGHT))
+         (player new-loc width height speed veloc turns)
+         (out-of-bounds))]
+    [_ pl]))
 
-; Turret -> Turret
-(define (rotate-turret tr)
-  tr)
+; Player Turret -> Turret
+(define (rotate-turret tr pl)
+  (define tpos (entity-coord tr))
+  (define ppos (entity-coord pl))
+  (struct-copy turret tr
+               [rotat (atan (- (posn-x tpos) (posn-x ppos))
+                            (- (posn-y tpos) (posn-y ppos)))]))
 
 ; Pvec -> Pvec
 ; rotates the quadrant in a cartesian plane a vector is in
@@ -267,14 +348,21 @@
   (match vec
     [(pvec x y) (pvec x (- y))]))
 
+; Game -> Boolean
+(define (game-over? g)
+  #f)
+
 ; -------------------------------------------------------------------------------------
 #| RENDERING |#
 ; -------------------------------------------------------------------------------------
-; Game -> Image
-(define (render-game g)
+; Game Images -> Image
+(define (render-game g img)
+  ;obstacles are static, is there a way I can prevent them from being re-rendered every
+  ;tick and simply "merge" them into the background image?
+  ; No! Because the background scrolls.
   (render-ship (game-p g)
                (render-turret (game-t g) (game-p g)
-                              (render-bg (game-p g) BACKG))))
+                              (overlay img (render-bg (game-p g) BACKG)))))
 
 ; Player Image -> Image
 (define (render-ui pl im)
@@ -286,7 +374,9 @@
 (define (render-ship pl im)
   (place-image
    (rotate (rationalize
-            (+ 90 (* (radians->degrees TURN-RATE) (player-turns pl))) .5) SHIP)
+            (* (radians->degrees TURN-RATE) (player-turns pl))
+            .5)
+           SHIP)
    (posn-x (entity-coord pl))
    (posn-y (entity-coord pl))
    im))
@@ -294,13 +384,22 @@
 ; Turret Image Player -> Image
 (define (render-turret tr pl im)
   (define crds (entity-coord tr))
-  (place-image (track crds TURRET (entity-coord pl)) (posn-x crds) (posn-y crds) im))
+  (place-image (track tr TURRET)
+               (posn-x crds) (posn-y crds)
+               im))
 
 ; Turret Image Posn -> Image
-(define (track tr im pl)
-  (rotate (- (radians->degrees (atan (- (posn-x tr) (posn-x pl))
-                                     (- (posn-y tr) (posn-y pl)))) 90)
+(define (track tr im)
+  (rotate (radians->degrees (turret-rotat tr))
           im))
+
+; [Listof Entity] Image -> Image
+(define (render-obst obstacles im)
+  (foldl (λ (an-entity img)
+           (match-define (entity (posn x y) width height) an-entity)
+           (place-image (rectangle width height 'solid 'red) x y img))
+         im
+         obstacles))
 
 ; Player Image -> Image
 (define (render-bg pl im)
@@ -318,14 +417,6 @@
 (define (difference n m)
   (abs (- n m)))
 
-; Posn Posn -> Number
-; determines the distance between two cartesian points. Using this instead
-; of taking the "straight" difference between x/y and 360 leads to some odd
-; results corresponding to sine/cosine motions around the circle.
-(define (distance point origin)
-  (sqrt (+ (sqr (- (posn-x point) (posn-x origin)))
-           (sqr (- (posn-x point) (posn-y origin))))))
-
 ; Image Number-> Image
 ; scrolls a background left by x and up by y
 ; given a background image, a current upper-left corner,
@@ -337,6 +428,14 @@
   ; - IN -
   (crop new-x new-y WIDTH HEIGHT bg))
 
+; Posn Posn -> Number
+; determines the distance between two cartesian points. Using this instead
+; of taking the "straight" difference between x/y and 360 leads to some odd
+; results corresponding to sine/cosine motions around the circle.
+(define (distance point origin)
+  (sqrt (+ (sqr (- (posn-x point) (posn-x origin)))
+           (sqr (- (posn-x point) (posn-y origin))))))
+
 ; [Listof Number] -> Image
 ; takes in a list of information, transforms it into strings, then appends
 ; labels to it, then flattens it into a single string and renders it.
@@ -346,11 +445,6 @@
                           (list "cur-x:" "cur-y:")
                           lo-str))
   (text (apply string-append label+info) 20 'black))
-#| (text
-(for/fold ([str_acc ""])
-          ([str-info (in-list info*)]
-           [str-label (in-list '("cur-x:" "cur-y:"))])
-  (string-append str-label " " (number->string str-info) " " str_acc)) 20 'black) |#
 
 ; Number X [Listof [X -> Y]] -> [Listof Benchmarks]
 (define (benchmark-funcs tm input . funcs)
@@ -358,5 +452,6 @@
     (displayln func)
     (time (for ([i (in-range tm)])
             (func input)))))
+
 #;(define start-game
     (thread start))
--- readme.md
+++ readme.md
@@ -4,29 +4,31 @@
 <ul>
 <li><a href="#orgheadline1">1. Boopo's Escape</a></li>
 <li><a href="#orgheadline2">2. Current progress:</a></li>
-<li><a href="#orgheadline3">3. <span class="todo nilTODO">TODO</span> <code>[1/5]</code></a></li>
-<li><a href="#orgheadline14">4. Data</a>
+<li><a href="#orgheadline3">3. <span class="todo nilTODO">TODO</span> <code>[4/7]</code></a></li>
+<li><a href="#orgheadline16">4. Data</a>
 <ul>
 <li><a href="#orgheadline4">4.1. Pvectors</a></li>
-<li><a href="#orgheadline7">4.2. Quadtree</a>
+<li><a href="#orgheadline8">4.2. Quadtree</a>
 <ul>
 <li><a href="#orgheadline5">4.2.1. Update to main module:</a></li>
 <li><a href="#orgheadline6">4.2.2. Multiple insertions into a quadtree</a></li>
+<li><a href="#orgheadline7">4.2.3. Rectangles vs. Square quad</a></li>
 </ul>
 </li>
-<li><a href="#orgheadline8">4.3. Entity</a></li>
-<li><a href="#orgheadline12">4.4. Player</a>
+<li><a href="#orgheadline9">4.3. Entity</a></li>
+<li><a href="#orgheadline13">4.4. Player</a>
 <ul>
-<li><a href="#orgheadline11">4.4.1. Notes</a></li>
+<li><a href="#orgheadline12">4.4.1. Notes</a></li>
 </ul>
 </li>
-<li><a href="#orgheadline13">4.5. Turret</a></li>
+<li><a href="#orgheadline14">4.5. Turret</a></li>
+<li><a href="#orgheadline15">4.6. Obstacles</a></li>
 </ul>
 </li>
-<li><a href="#orgheadline17">5. Rendering</a>
+<li><a href="#orgheadline19">5. Rendering</a>
 <ul>
-<li><a href="#orgheadline15">5.1. Progress:</a></li>
-<li><a href="#orgheadline16">5.2. Scrolling</a></li>
+<li><a href="#orgheadline17">5.1. Progress:</a></li>
+<li><a href="#orgheadline18">5.2. Scrolling</a></li>
 </ul>
 </li>
 </ul>
@@ -41,15 +43,17 @@ This is a small, simple project. You fly a spaceship from one end of the screen 
 
 A ship and turret can be rendered on the background. You can fly the ship around the screen. You turn in increments of π/6, but this can be modified to any arbitrary radian unit.
 
-# TODO <code>[1/5]</code><a id="orgheadline3"></a>
+# TODO <code>[4/7]</code><a id="orgheadline3"></a>
 
 -   [X] Determine primary data structure.
--   [ ] Implement simple quadtree for collision-checking between entities (projectiles, turret, ship).
--   [ ] Add the turret.
--   [ ] Add turret tracking.
+-   [X] Implement simple quadtree for collision-checking between entities (projectiles, turret, ship).
+-   [X] Add the turret.
+-   [X] Add turret tracking.
 -   [ ] Implement three-layer rendering.
+-   [ ] Map the absolute coordinates of entities on the background image to the relative coordinates of the screen and the quadtree.
+-   [ ] Refactor turret tracking from a purely visual effect to a part of the turret's data structure (will have to extend turret's base entity struct with another one)
 
-# Data<a id="orgheadline14"></a>
+# Data<a id="orgheadline16"></a>
 
 ## Pvectors<a id="orgheadline4"></a>
 
@@ -58,7 +62,7 @@ A 2-dimensional vector, `(pvector Integer Integer)` in `(pvector x y)` can repre
 -   **x:** the ship's velocity in the horizontal plane, or its horizontal distance from the origin
 -   **y:** the ship's velocity in the vertical plane, or its vertical distance from the origin
 
-## Quadtree<a id="orgheadline7"></a>
+## Quadtree<a id="orgheadline8"></a>
 
 `(node Posn [Listof Any] (U Empty [List Node Node Node Node]))`
 in `(node coord content children)`
@@ -87,7 +91,11 @@ information in both the game struct AND the quadtree.
 
 I could add support for inserting multiple items during one walk (instead of having to walk the tree once per insertion) by using a folding or accumulated function. The accumulator represents the tree so far, and the function consumes an arbitrary list of things to insert. Every node you check if any entity fits into a child-node and insert it appropriately.
 
-## Entity<a id="orgheadline8"></a>
+### Rectangles vs. Square quad<a id="orgheadline7"></a>
+
+Note, I'm going to have to reconfigure the quadtree module to use rectangles instead of squares. The ship is rectangle, and the obstacles on the map are also rectang
+
+## Entity<a id="orgheadline9"></a>
 
 `(entity Posn Number Number)`
 in `(entity coord width height`
@@ -98,7 +106,7 @@ in `(entity coord width height`
 
 Right now I'm planning on having the player's struct extend the entity struct with magnitude and rotation.
 
-## Player<a id="orgheadline12"></a>
+## Player<a id="orgheadline13"></a>
 
 `(player Natural Pvector Pvector Natural)` 
 in `(player m v l t)`
@@ -108,7 +116,7 @@ in `(player m v l t)`
 -   **l:** A Pvector representing the location of the ship as the distance from the origin, the upper left, of the screen.
 -   **t:** The number of turns around the circle in "n" radian units. Used for rendering the ship at the appropriate angle and for rotating the ship during play.
 
-### Notes<a id="orgheadline11"></a>
+### Notes<a id="orgheadline12"></a>
 
 1.  The controls are fine now, but previously:
 
@@ -116,7 +124,7 @@ in `(player m v l t)`
 
 2.  I think I might have to make the player mutable. Right now the player is constantly being reinserted into the quadtree, and the quadtree constantly being rebuilt, multiple times every second. It's easier to set! the player's new location and speed, and only re-insert every time it moves to a new quadrant or new objects are introduced.
 
-## Turret<a id="orgheadline13"></a>
+## Turret<a id="orgheadline14"></a>
 
 The turret hasn't been implemented yet, here are some ideas:
 
@@ -125,17 +133,25 @@ The turret hasn't been implemented yet, here are some ideas:
 -   After recording three such locations, it determines a best-fit linear function between the three points (such as in a scatter-plot).
 -   It then fires at a location f(x), taking into account the player's speed along.
 
-# Rendering<a id="orgheadline17"></a>
+## Obstacles<a id="orgheadline15"></a>
+
+Right now there are no obstacles to hide behind or collide into. How would I implement obstacles?
+
+-   Random rectangles scattered across the screen.
+
+Rendering obstacles is going to be kind of difficult. I can't just slap them into a 720 by 720 square, because that square scrolls as the player moves. Maybe I can generate obstacles in a square surrounding the turret? The further away you are, the less cover there is.
+
+# Rendering<a id="orgheadline19"></a>
 
 I think I'll represent the game visually as a three-layered image. The bottom image is the background (land masses, some clouds, water, space, etc.). The second image contains the ships, obstacles, and turrets. The third image might have more clouds.
 I'll look into parallax scrolling too, which could be scaled by the player's magnitude, or maybe even the player's angle.
 
-## Progress:<a id="orgheadline15"></a>
+## Progress:<a id="orgheadline17"></a>
 
 Renders by overlaying the ship over the turret over the background.
 The background is rendered by "scrolling" a larger image via successive cropping around the player.
 
-## Scrolling<a id="orgheadline16"></a>
+## Scrolling<a id="orgheadline18"></a>
 
 Takes a background image, the current coordinates of the upper-left corner, and the rate at which the background scrolls by x- and y-coords. It produces a new image, cropped to the dimensions of the screen such that the player is centered, with the background "scrolled" in the opposite direction by the player's speed.
 The initial coordinates are determined by a constant, which is the coordinate location of the viewport on the background screen. As the player moves around the screen, the background is scrolled by their speed per tick.
